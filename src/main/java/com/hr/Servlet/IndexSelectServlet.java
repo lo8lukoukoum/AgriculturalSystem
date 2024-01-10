7ddc9533c905840d721185a247b904ef9f7dcdb9
Godspeed <2632533923@qq.com>
2024-01-10 19:07:34 +0800

    1

--- src/main/java/com/hr/Servlet/IndexSelectServlet.java
+++ src/main/java/com/hr/Servlet/IndexSelectServlet.java
@@ -1,6 +1,5 @@
 package com.hr.Servlet;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -22,24 +21,39 @@ public class IndexSelectServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		//查询分类数据
+
+		// 从数据库查询分类数据
 		ArrayList<EASYBUY_PRODUCT_CATEGORY> flist = EASYBUY_PRODUCT_CATEGORYDao.selectFather();
 		req.setAttribute("flist", flist);
+
+		// 从数据库查询子分类数据
 		ArrayList<EASYBUY_PRODUCT_CATEGORY> clist = EASYBUY_PRODUCT_CATEGORYDao.selectChild();
 		req.setAttribute("clist", clist);
+
+		// 从数据库查询热门商品
 		ArrayList<EASYBUY_PRODUCT> tlist = EASYBUY_PRODUCTDao.selectAllByT();
 		req.setAttribute("tlist", tlist);
+
+		// 从数据库查询最新商品
 		ArrayList<EASYBUY_PRODUCT> hlist = EASYBUY_PRODUCTDao.selectAllByHot();
 		req.setAttribute("hlist", hlist);
+
+		// 从数据库查询最新消息
 		ArrayList<EASYBUY_NEWS> nlist = EASYBUY_NEWSDao.selectAll();
 		req.setAttribute("nlist", nlist);
+
+		// 获取用户session
 		HttpSession session = req.getSession();
-		//查询最近浏览的商品
+
+		// 从session中获取最近浏览的商品id
 		ArrayList<Integer> ids = (ArrayList<Integer>)session.getAttribute("ids");
 		if(ids!=null){
+			// 根据id查询最近浏览的商品信息
 			ArrayList<EASYBUY_PRODUCT> lastlylist = EASYBUY_PRODUCTDao.selectById(ids);
 			req.setAttribute("lastlylist", lastlylist);
 		}
+
+		// 转发请求到index.jsp
 		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
 }
